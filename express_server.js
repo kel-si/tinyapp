1bf6bc3d287326bcbd39df63366a3826c9a9c2e5
Kelsi Olstad <kelsiolstad@gmail.com>
2022-02-22 15:03:03 -0700

    redirect shortURL to longURL

--- express_server.js
+++ express_server.js
@@ -66,6 +66,13 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${shortURL}`);
 });
 
+
+//redirect to long url
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+})
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port: ${PORT}`);
 });
