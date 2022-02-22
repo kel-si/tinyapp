b1679ae47096f061051ee0be8f6073158d600bb7
Kelsi Olstad <kelsiolstad@gmail.com>
2022-02-21 21:00:07 -0700

    add route

--- express_server.js
+++ express_server.js
@@ -36,6 +36,10 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = {shortURL: req.params.shortURL, longURL: req.params.longURL};
+  res.render("urls_show", templateVars);
+});
 
 
 app.listen(PORT, () => {
