f6cb90b8c4794973d57288b29058cd5b1b7f47e8
Kelsi Olstad <kelsiolstad@gmail.com>
2022-02-22 19:55:15 -0700

    update templateVars to include username: req.cookies

--- express_server.js
+++ express_server.js
@@ -49,7 +49,7 @@ app.get("/hello", (req, res) => {
 
 //pass URL data to template
 app.get("/urls", (req, res) => {
-  const templateVars = {urls: urlDatabase};
+  const templateVars = {username: req.cookies["username"], urls: urlDatabase};
   res.render("urls_index", templateVars);
 });
 
@@ -59,7 +59,7 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  const templateVars = {username: req.cookies["username"], shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
 });
 
