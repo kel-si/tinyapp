2d57c4c7a1f5c8d5cabd3a271068af75a77e5beb
Kelsi Olstad <kelsiolstad@gmail.com>
2022-02-24 10:26:52 -0700

    correct generateRandomString to 6 characters

--- helpers.js
+++ helpers.js
@@ -1,14 +1,14 @@
 const { get } = require("express/lib/response");
 
 const generateRandomString = function() {
-  const randomKey =  Math.random().toString(36).substring(6);
+  const randomKey =  Math.random().toString(36).substring(6).slice(1);
   return randomKey;
 };
 
 const getUserByEmail = function (obj, email) {
   for(let key in obj) {
     if (obj[key].email === email) {
-      return obj[key];
+      return obj[key].email;
     }
   }
   return null;
